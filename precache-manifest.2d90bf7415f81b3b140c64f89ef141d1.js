d5daaef74db35f16c1ae0ab6609808e358feb87f
NIHAL TIWARI <nihaltiwari28@gmail.com>
2020-10-11 11:35:52 -0700

    Updates

---
+++ precache-manifest.2d90bf7415f81b3b140c64f89ef141d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e06ffbeb26065331b14bf66c27b765",
+    "url": "/internshala/index.html"
+  },
+  {
+    "revision": "d05644e97e06263c7024",
+    "url": "/internshala/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "5a1f1e92ad1c97253812",
+    "url": "/internshala/static/css/main.a11da77e.chunk.css"
+  },
+  {
+    "revision": "d05644e97e06263c7024",
+    "url": "/internshala/static/js/2.c8c372e9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/internshala/static/js/2.c8c372e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1f1e92ad1c97253812",
+    "url": "/internshala/static/js/main.5b595f13.chunk.js"
+  },
+  {
+    "revision": "bd24a495a3e72ee0db60",
+    "url": "/internshala/static/js/runtime-main.1f6ffdb9.js"
+  }
+]);
